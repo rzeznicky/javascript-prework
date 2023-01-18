aeca6964a43ea109e3168bfdbe6c284be398e4be
Pawel Rzeznicki <p.rzeznicky@gmail.com>
2023-01-18 22:08:42 +0100

    Add var playerMove to js/script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var computerMove;
+var playerMove;
+computerMove = 'kamień';
+playerMove = 'papier';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ' to wygrywasz!' + '' + '');
